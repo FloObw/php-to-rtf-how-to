470f12109cc85c004b5998d6cdc3a56943798c59
vipertex@gmx.net <vipertex@gmx.net>
2013-04-09 23:09:07 +0200

    format source code

--- rtf_example.php
+++ rtf_example.php
@@ -1,10 +1,9 @@
-  <?php
+<?php
 	include('/lib/rtfclass.php');
 
-        $text = new rtf ('<strong>This is my first blind text</strong>');
-        $text->output("html");
-        $text->parse();
-        if( count( $text->err) == 0) { // no errors detected
+	$text = new rtf ('<strong>This is my first blind text</strong>');
+	$text->output("html");
+	$text->parse();
+	if( count( $text->err) == 0) { // no errors detected
 		echo textr->out;
-        }
-    ?> 
+	}
