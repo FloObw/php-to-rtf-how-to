0faaf3ef47dbc90aea02d7cfe83e867a6bfa8327
FloObw <florian.obwegs@hotmail.com>
2013-04-10 15:29:49 +0300

    Update rtf_example.php

--- rtf_example.php
+++ rtf_example.php
@@ -1,7 +1,7 @@
 <?php
 	include('/lib/rtfclass.php');
 
-	$text = new rtf ('<strong>This is my first blind text</strong>');
+	$text = new rtf ($rtfFile);
 	$text->output("html");
 	$text->parse();
 	if( count( $text->err) == 0) { // no errors detected
